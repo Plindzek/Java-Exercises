19c3d1d19e80133a5a3aef035e0cd158bd04009a
Adam <Plindzek@users.noreply.github.com>
2019-11-30 13:29:37 +0100

    removed wrong code

--- pmaniw/src/Player.java
+++ pmaniw/src/Player.java
@@ -14,28 +14,7 @@ public class Player {
 File mp3 = new File("mp3Name");
     
     void play(File mp3) {
-	byte[] data;
-	InputStream in = ClassLoader.getSystemResourceAsStream("D:\\Projekty Java\\pliki\\1.mp3"); //in an applet
-	AudioInputStream ais = AudioSystem.getAudioInputStream(in);
-	data = new byte[(int)ais.getFrameLength()];
-	byte[] buf = new byte[BUFSIZE];
-	for (int i=0; i<data.length; i+=BUFSIZE) {
-	    int r = ais.read(buf, 0, BUFSIZE);
-	    if (i+r >= data.length) {
-	        r = data.length - i;
-	    }
-	    System.arraycopy(buf, 0, data, i, r);
-	}
-	ais.close();
-	line.start();
-	for (int i=0; i<data.length; i+=BUFSIZE) {
-	    line.write(data, i, BUFSIZE);
-	}
-	line.drain();
-	line.stop();
 	
-	/* open stream with buffer and send to System.out.audio
-	*/
     }
     void stop(Stream st) {
 	//stream.close
