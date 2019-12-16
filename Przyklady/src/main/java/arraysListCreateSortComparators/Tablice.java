22d6e75c4f9f1f16e810aba1e1148837ec16a405
Adam <Plindzek@users.noreply.github.com>
2019-12-16 10:36:26 +0100

    fix encoding

--- Przyklady/src/main/java/arraysListCreateSortComparators/Tablice.java
+++ Przyklady/src/main/java/arraysListCreateSortComparators/Tablice.java
@@ -3,7 +3,7 @@ package arraysListCreateSortComparators;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class Tablice { // przyk³ad tablicy i wype³niania jej za pomoc¹ pêtli for
+public class Tablice { // przyklad tablicy i wypelniania jej za pomoca petli for
 
     public static void main(String[] args)
 
@@ -17,17 +17,17 @@ public class Tablice { // przyk³ad tablicy i wype³niania jej za pomoc¹ pêtli for
 	
 	int[] tablica = new int[10];
 
-	for (int i = 0; i < 10; i++) // nie dawaæ œrednika za nawiasem!!!
-	    tablica[i] = i; // wype³nianie tablicy za pomoc¹ pêtli
+	for (int i = 0; i < 10; i++) // nie dawac srednika za nawiasem!!!
+	    tablica[i] = i; // wypelnianie tablicy za pomoca petli
 
 	for (int i = 0; i < 10; i++)
 
-	    System.out.println("Kolejna komórka to: " + tablica[i]);
+	    System.out.println("Kolejna komorka to: " + tablica[i]);
 
 	String[][] tablicaWielowymiarowa = new String[3][3];
 	tablicaWielowymiarowa[1][2] = "Ala";
 
-	System.out.println("komórka o adresie [1][2]to: " + tablicaWielowymiarowa[1][2]);
+	System.out.println("komorka o adresie [1][2]to: " + tablicaWielowymiarowa[1][2]);
 
 	HashMap<Integer, String> liczby = new HashMap<Integer, String>(0, 0.1f);
 
