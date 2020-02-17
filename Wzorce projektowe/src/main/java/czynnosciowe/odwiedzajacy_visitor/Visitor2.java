d5a7e2feb430a93e6e6e1542c5adc1fffa9d31df
Adam <Plindzek@users.noreply.github.com>
2020-02-17 14:58:42 +0100

    add toString and explanation how it works

--- Wzorce projektowe/src/main/java/czynnosciowe/odwiedzajacy_visitor/Visitor2.java
+++ Wzorce projektowe/src/main/java/czynnosciowe/odwiedzajacy_visitor/Visitor2.java
@@ -6,13 +6,22 @@ public class Visitor2 extends Visitor{
     public void visitElement(
 	    ElementA elementA)
     {
-        System.out.println(elementA.getClass().getName()+ " visited by " + getClass().getName());
+	System.out.println(elementA + " visited by " + this
+		+ "\n (it means method in data structure calls element method, who calls"
+		+ " one of visitor overload methods who do something with this element)");
     }
 @Override
     public void visitElement(
       ElementB elementB)
     {
-        System.out.println(elementB.getClass().getName()+ " visited by " + getClass().getName());
+	System.out.println(elementB + " visited by " + this
+		+ "\n (it means method in data structure calls element method, who calls"
+		+ " one of visitor overload methods who do something with this element)");
+    }
+
+    @Override
+    public String toString() {
+	return "Visitor2";
     }
 
 }
