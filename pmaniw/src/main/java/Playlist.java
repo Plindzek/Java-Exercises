17282fedf9b809cdc44183865eca9b91d9b32b1f
Adam <Plindzek@users.noreply.github.com>
2020-05-27 20:38:52 +0200

    del

--- pmaniw/src/main/java/Playlist.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Playlist {
-   
-    List<Song> playlist = new ArrayList<Song>();
-    void addSong() {}
-    void removeSong() {}
-}
